344b3f7384eab4eea258009a1b6cd9db059dd431
Rajib <rajib@ubuntu>
2023-11-05 22:50:18 +0600

    navbar added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../shared/Footer/Footer";
+import Navbar from "../shared/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
-            
+            <Navbar />
             <div className="mx-auto max-w-7xl">
             <Outlet />
             </div>
