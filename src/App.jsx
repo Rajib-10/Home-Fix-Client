3636cf7b2e94f24b3e7bc80f456e7e856b46cd39
Rajib <rajib@ubuntu>
2023-11-05 17:57:01 +0600

    Environment setup using react router and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      
+        
+      <h1 className="text-4xl text-orange-500 text-center">Hello Home-Fix </h1>
+      
+    </>
+  )
+}
+
+export default App
